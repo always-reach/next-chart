2b9a378831ece9c7e7f2d7af7c1a2c560c54ace1
always-reach <bythway811@gmail.com>
2024-03-17 00:50:46 +0900

    棒グラフ

--- app/_components/BarChart.tsx
+++ app/_components/BarChart.tsx
@@ -2,9 +2,15 @@
 import * as d3 from 'd3';
 import React, { useEffect, useRef } from 'react';
 
+type Data = {
+  name: string;
+  value: number;
+};
+
+
 type Props= {
   title: string;
-  data: number[];
+  data: Data[];
   width?: number;
   height?: number;
   color?: string;
@@ -18,41 +24,37 @@ const BarChart: React.FC<Props> = ({ title, data, width = 640, height = 400, col
 
   useEffect(() => {
     const svg = d3.select(ref.current);
-    const xScale = d3.scaleLinear().domain([0, data.length - 1]).range([0, width]);
-    const yScale = d3.scaleLinear().domain([0, d3.max(data) ?? 0]).range([height, 0]);
+    svg.selectAll('*').remove();
+    const margin = {top: 20, right: 30, bottom: 40, left: 90}
+    const xScale = d3.scaleBand().domain(data.map(d=>d.name)).range([0, width]).padding(0.3);
+    const yScale = d3.scaleLinear().domain([0, d3.max(data.map(d=>d.value))??0]).range([height, 0]);
 
-    const lineGenerator = d3.line<number>()
-      .x((_d, i) => xScale(i))
-      .y(d => yScale(d));
+    const g = svg.append('g').attr('transform', `translate(${margin.left},${margin.top})`);
 
-    svg.select('.line-path')
-      .attr('d', lineGenerator(data))
-      .attr('stroke', color)
-      .attr('stroke-width', strokeWidth)
-      .attr('fill', fill);
+    g.append('g').call(d3.axisLeft(yScale));
 
-    // アニメーションの追加
-    svg.select('.line-path')
-      .attr('stroke-dasharray', function() {
-        const path = this as SVGPathElement;
-        const length = path.getTotalLength();
-        return `${length} ${length}`;
-      })
-      .attr('stroke-dashoffset', function() {
-        const path = this as SVGPathElement;
-        return path.getTotalLength();
-      })
+    g.append('g').attr('transform', `translate(0,${innerHeight})`).call(d3.axisBottom(xScale));
+
+    g.selectAll('.bar')
+      .data(data)
+      .enter().append('rect')
+      .attr('class', 'bar')
+      .attr('x', d => xScale(d.name)!)
+      .attr('y', innerHeight)
+      .attr('width', xScale.bandwidth())
+      .attr('fill', color)
       .transition()
-      .duration(2000)
-      .attr('stroke-dashoffset', 0);
+      .duration(750)
+      .attr('y', d => yScale(d.value))
+      .attr('height', d => innerHeight - yScale(d.value));
+
+
   }, [data, width, height, color, strokeWidth, fill]);
 
   return (
     <div>
       <h2>{title}</h2>
-      <svg ref={ref} width={width} height={height}>
-        <path className="line-path" />
-      </svg>
+      <svg ref={ref} width={width} height={height} />
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div>
       <LineChart title="テスト用の折れ線グラフ" data={[10, 9, 3, 8, 15, 43, 30, 26, 19, 35]} />
-      <BarChart title="テスト用の棒グラフ" data={[10, 9, 3, 8, 15, 43, 30, 26, 19, 35]} />
+      <BarChart title="テスト用の棒グラフ" data={[{name:'a',value:10},{name:'b',value:3},{name:'c',value:30}]} />
     </div>
   );
 }
